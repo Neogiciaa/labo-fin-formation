b28575c0fcff916d49efd08a434cadea9decff3c
MikeTheBug <bastin.mike1@gmail.com>
2023-02-09 12:11:13 +0100

    userRouter updated

--- routers/user.router.js
+++ routers/user.router.js
@@ -7,29 +7,16 @@ const authentificate = require('../middlewares/authentificate.middleware');
 
 const userRouter = require('express').Router();
 
-// Récupération de tous les utilisateurs de la DB via le controller (recherche et ajout d'un ami)
+// Récupération de tous les utilisateurs de la DB
 userRouter.get('/', authentificate(), userController.getAll)
 
-// Récupération d'un utilisateur via son id pour afficher chaque utilisateur présent dans la liste d'ami
+// Récupération d'un utilisateur via son id pour afficher ses infos
 userRouter.get('/:id', authentificate(), userController.getById)
-
-// Ajout d'un ami
-userRouter.post('/', authentificate(), userController.addFriend)
 
 // Mise à jour des données personnelles du profil utilisateur
 userRouter.put('/:id', authentificate(), bodyValidation(profilOptionsValidator), userController.update)
 
-// Accepter les demandes d'amis !
-userRouter.put('/friend/:id', authentificate(), userController.acceptFriendRequest)
-
-// Refuser la demande d'amis !
-userRouter.put('/friend/:id', authentificate(), userController.declineFriendRequest)
-
-
-// Supprimer un ami de sa liste d'ami
-userRouter.delete('/friend/:id', authentificate(), userController.deleteFriend);
-
-// Route ADMIN : suppression d'un compte utilisateur de l'application globable
+// Suppression d'un compte utilisateur de l'application globable
 userRouter.delete('/:id', authentificate(), userController.delete);
 
 
